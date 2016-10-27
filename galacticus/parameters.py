6aae3dd37d84e5f97a8bd49b2830483dd592dfac
Alex Merson <alex.i.merson@gmail.com>
2016-10-26 17:10:09 -0700

    Updated parameters.py to work with xmlTree.py. (Will likely be some bugs inserted as a result!)

--- galacticus/parameters.py
+++ galacticus/parameters.py
@@ -1,14 +1,94 @@
 #! /usr/bin/env python
 
-import os,sys
+import os,sys,fnmatch
 import numpy as np
 import xml.etree.ElementTree as ET
+from .xmlTree import xmlTree
 
 ######################################################################################
 # PARAMETERS
 ######################################################################################
 
-class GalacticusParameters(object):
+
+class GalacticusParameters(xmlTree):
+    
+    def __init__(self,xmlfile,verbose=False):
+        classname = self.__class__.__name__
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        super(GalacticusParameters,self).__init__(xmlfile=xmlfile,verbose=verbose)
+        return
+    
+    def getParameter(self,param):
+        """
+        get_parameter: Return value of specified parameter.
+        
+        USAGE: value = getParameter(param)
+
+            INPUT
+                param -- name of parameter
+            OUTPUT
+                value -- string with value for parameter
+
+        """
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name        
+        if not param in self.treeMap.keys():
+            if self._verbose:
+                print("WARNING! "+funcname+"(): Parameter '"+\
+                          param+"' cannot be located in "+self.xmlfile)
+            value = None
+        else:
+            path = self.treeMap[param]
+            elem = self.getElement(path)
+            value = elem.attrib.get("value")
+        return value
+
+
+    def getParent(self,param):
+        """
+        getParent: Return name of parent element.
+
+        USAGE: name = getParent(elem)
+
+            INPUT
+                elem -- name of current element
+            OUTPUT
+                name -- string with name of parent
+
+        """
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        if not param in self.treeMap.keys():
+            if self._verbose:
+                print("WARNING! "+funcname+"(): Parameter '"+\
+                          param+"' cannot be located in "+self.xmlfile)
+            name = None
+        else:
+            name = self.treeMap[param]
+            name = name.split("/")[-2]
+        return name
+
+
+    def constructDictionary(self):
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        params = {}
+        for e in self.treeMap.keys():
+            params[e] = self.getParameter(e)
+        return params
+
+    
+    def setParameter(self,param,value,parent=None,selfCreate=False):        
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        # Convert paramter value to string
+        if np.ndim(value) == 0:
+            value = str(value)
+        else:
+            value = " ".join(map(str,value))
+        # Set parameter
+        self.setElement(param,attrib={"value":value},parent=parent,\
+                            selfCreate=selfCreate)
+        return
+
+
+class GalacticusParametersOld(object):
     
     def __init__(self,xmlfile,verbose=False):
         classname = self.__class__.__name__
