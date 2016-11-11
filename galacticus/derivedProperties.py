a9d966c518b118da937c839953fff251d1915cf9
Alex Merson <alex.i.merson@gmail.com>
2016-11-10 17:46:29 -0800

    In derivedProperties.py, moved addDerivedProperties() function into a class, derivedProperties, with function derivedProperties.addProperties().

--- galacticus/derivedProperties.py
+++ galacticus/derivedProperties.py
@@ -4,63 +4,75 @@ import sys,fnmatch
 import numpy as np
 from .io import GalacticusHDF5
 from .utils.progress import Progress
+from .EmissionLines import GalacticusEmissionLines
 
-def addDerivedProperties(galHDF5Obj,z,derivedDatasets,overwrite=False,verbose=True):
-    funcname = sys._getframe().f_code.co_name
+class derivedProperties(object):
+    
+    def __init__(self,galHDF5Obj,verbose=True):        
+        classname = self.__class__.__name__
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        self.galHDF5Obj = galHDF5Obj
+        self.EmissionLines = None
+        self._verbose = verbose
+        return
 
+    def addProperties(self,z,derivedDatasets,overwrite=False):
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
 
-    PROG = None
-    if verbose:
-        print(funcname+"(): processing derived datasets:")       
-    # Stellar masses
-    datasets = fnmatch.filter(derivedDatasets,"*MassStellar")
-    if len(datasets)>0: 
-        from .Stars import getStellarMass
-        if verbose:
-            print("    stellar mass...")
-            PROG = Progress(len(datasets))
-        dummy = [getStellarMass(galHDF5Obj,z,name,overwrite=overwrite,\
-                                    returnDataset=False,progressObj=PROG) for name in datasets]
-        del dummy    
-    # Star formation rates
-    datasets = fnmatch.filter(derivedDatasets,"*StarFormationRate")
-    if len(datasets)>0: 
-        from .Stars import getStarFormationRate
-        if verbose:
-            print("    star formation rate...")
-            PROG = Progress(len(datasets))
-        dummy = [getStarFormationRate(galHDF5Obj,z,name,overwrite=overwrite,\
-                                          returnDataset=False,progressObj=PROG) for name in datasets]
-        del dummy
-    # Stellar luminosities 
-    datasets = fnmatch.filter(derivedDatasets,"*LuminositiesStellar:*")
-    datasets = list(set(datasets).difference(fnmatch.filter(datasets,"*:dust*")))
-    if len(datasets)>0: 
-        from .Stars import getLuminosity
-        if verbose:
-            print("    stellar luminosity...")
-            PROG = Progress(len(datasets))
-        dummy = [getLuminosity(galHDF5Obj,z,name,overwrite=overwrite,\
-                                   returnDataset=False,progressObj=PROG) for name in datasets]
-        del dummy
-    # Emission line luminosities
-    datasets = fnmatch.filter(derivedDatasets,"*LineLuminosity:*")
-    if len(datasets)>0: 
-        from .EmissionLines import GalacticusEmissionLines
-        if verbose:
-            print("    cold gas...")
-            PROG = Progress(len(datasets))
-        dummy = [GalacticusEmissionLines().getLineLuminosity(galHDF5Obj,z,name,overwrite=overwrite,\
-                                                                 returnDataset=False,progressObj=PROG) for name in datasets]
-        del dummy
-    # Cold gas
-    datasets = fnmatch.filter(derivedDatasets,"*MassGas") + fnmatch.filter(derivedDatasets,"*MassColdGas")
-    if len(datasets)>0: 
-        from .ColdGas import getColdGasMass        
-        if verbose:
-            print("    cold gas...")
-            PROG = Progress(len(datasets))
-        dummy = [getColdGasMass(galHDF5Obj,z,name,overwrite=overwrite,\
-                                    returnDataset=False,progressObj=PROG) for name in datasets]
-        del dummy
+        PROG = None
+        if self._verbose:
+            print(funcname+"(): processing derived datasets:")       
+            
+        # Stellar masses
+        datasets = fnmatch.filter(derivedDatasets,"*MassStellar")
+        if len(datasets)>0: 
+            from .Stars import getStellarMass
+            if self._verbose:
+                print("    stellar mass...")
+                PROG = Progress(len(datasets))
+            dummy = [getStellarMass(self.galHDF5Obj,z,name,overwrite=overwrite,\
+                                        returnDataset=False,progressObj=PROG) for name in datasets]
+            del dummy    
+        # Star formation rates
+        datasets = fnmatch.filter(derivedDatasets,"*StarFormationRate")
+        if len(datasets)>0: 
+            from .Stars import getStarFormationRate
+            if self._verbose:
+                print("    star formation rate...")
+                PROG = Progress(len(datasets))
+            dummy = [getStarFormationRate(self.galHDF5Obj,z,name,overwrite=overwrite,\
+                                              returnDataset=False,progressObj=PROG) for name in datasets]
+            del dummy
+        # Stellar luminosities 
+        datasets = fnmatch.filter(derivedDatasets,"*LuminositiesStellar:*")
+        datasets = list(set(datasets).difference(fnmatch.filter(datasets,"*:dust*")))
+        if len(datasets)>0: 
+            from .Stars import getLuminosity
+            if self._verbose:
+                print("    stellar luminosity...")
+                PROG = Progress(len(datasets))
+            dummy = [getLuminosity(self.galHDF5Obj,z,name,overwrite=overwrite,\
+                                       returnDataset=False,progressObj=PROG) for name in datasets]
+            del dummy
+        # Emission line luminosities
+        datasets = fnmatch.filter(derivedDatasets,"*LineLuminosity:*")
+        if len(datasets)>0:         
+            if self.EmissionLines is None:
+                self.EmissionLines = GalacticusEmissionLines()
+            if self._verbose:
+                print("    emission line luminosities...")
+                PROG = Progress(len(datasets))
+            dummy = [self.EmissionLines.getLineLuminosity(self.galHDF5Obj,z,name,overwrite=overwrite,\
+                                                              returnDataset=False,progressObj=PROG) for name in datasets]
+            del dummy
+        # Cold gas
+        datasets = fnmatch.filter(derivedDatasets,"*MassGas") + fnmatch.filter(derivedDatasets,"*MassColdGas")
+        if len(datasets)>0: 
+            from .ColdGas import getColdGasMass        
+            if self._verbose:
+                print("    cold gas...")
+                PROG = Progress(len(datasets))
+            dummy = [getColdGasMass(self.galHDF5Obj,z,name,overwrite=overwrite,\
+                                        returnDataset=False,progressObj=PROG) for name in datasets]
+            del dummy
 
