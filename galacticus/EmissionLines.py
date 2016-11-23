In SED.py corrected unit conversion of emission line luminosities.

galacticus/EmissionLines.py
@@ -202,11 +202,14 @@ class GalacticusEmissionLines(object):
         # Convert to line luminosity in Solar luminosities (or AB maggies if a filter was specified)
         lineLuminosity *= luminosityMultiplier*numberHIIRegion*erg/luminositySolar
         np.place(lineLuminosity,OR(np.invert(hasGas),np.invert(hasSize),np.invert(hasFlux),np.invert(formingStars)),0.0)
+        np.place(lineLuminosity,lineLuminosity<0.0,-999.9)
+        np.place(lineLuminosity,np.isnan(lineLuminosity),-999.9)
+        np.place(lineLuminosity,np.isinf(lineLuminosity),-999.9)
         # Add luminosity to file and return values
         galHDF5Obj.addDataset(out.name+"/nodeData/",datasetName,np.copy(lineLuminosity))
         # Add appropriate attributes to new dataset
         attr = {"unitsInSI":luminositySolar}
-        galHDF5Obj.addAttributes(out.name+"/nodeData/"+datasetName,attr)
+        galHDF5Obj.addAttributes(out.name+"/nodeData/"+datasetName,attr)        
         return lineLuminosity
 
 

galacticus/SED.py
@@ -6,7 +6,7 @@ from scipy.interpolate import interp1d
 from galacticus.io import GalacticusHDF5
 from galacticus.EmissionLines import GalacticusEmissionLines
 from galacticus.Luminosities import ergPerSecond
-from galacticus.constants import erg,luminosityAB,jansky
+from galacticus.constants import erg,luminosityAB,luminositySolar,jansky
 from galacticus.constants import angstrom,megaParsec,Pi,speedOfLight,centi
 
 
@@ -100,12 +100,20 @@ class GalacticusSED(object):
                 lineLuminosity = np.array(out["nodeData/"+lineDatasetName])
                 if selectionMask is not None:
                     lineLuminosity = lineLuminosity[selectionMask]
-                luminosities[:,iarg] += lineLuminosity
+                np.place(lineLuminosity,lineLuminosity<0.0,0.0)
+                luminosities[:,iarg] += lineLuminosity*(luminositySolar/luminosityAB)
         dummy = [_addLuminosity(linesInRange[i],wavelengthsInRange[i]) for i in range(len(linesInRange))]
         del dummy
         return wavelengths,luminosities        
         
 
+
+    def ergPerSecond(self,luminosity):
+        luminosity = np.log10(luminosity)
+        luminosity += np.log10(luminosityAB)
+        luminosity -= np.log10(erg)
+        luminosity = 10.0**luminosity
+        return luminosity
         
     def getSED(self,datasetName,selectionMask=None,includeEmissionLines=True):
         funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
@@ -140,7 +148,7 @@ class GalacticusSED(object):
         if includeEmissionLines:
             wavelengths,sed = self.addEmissionLines(wavelengths,sed,MATCH,selectionMask=selectionMask)            
         # Convert units to microJanskys
-        sed = ergPerSecond(sed)
+        sed = self.ergPerSecond(sed)
         comDistance = self.galacticusOBJ.cosmology.comoving_distance(redshift)*megaParsec/centi
         sed /= 4.0*Pi*comDistance**2
         frequency = speedOfLight/np.stack([wavelengths]*ngals)*angstrom